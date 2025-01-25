e52d9b16b9ab984ac0bfe594767011d68edbf931
IkerSaint <iker.saint@gmail.com>
2025-01-25 00:51:52 -0500

    Linter and other checks

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # Action
 
-A GitHub action that retrieves and outputs the current repository languages using [List repository languages](https://docs.github.com/en/rest/repos/repos#list-repository-languages) API.
+A GitHub Action that retrieves and outputs the current repository languages
+using
+[List repository languages](https://docs.github.com/en/rest/repos/repos#list-repository-languages)
+API.
 
 ## Usage
+
 Create a workflow `.github/workflows/languages.yml`.
 
 ### Example
+
 Print all the languages detected.
+
 ```yml
 name: Echo Languages
 on:
@@ -18,17 +24,21 @@ jobs:
     steps:
       - uses: IkerSaint/get_repo_languages@main
         id: get-languages
-      - run: echo ${{ join(fromJSON(steps.get-languages.outputs.languages), ',') }}
+      - run:
+          echo ${{ join(fromJSON(steps.get-languages.outputs.languages), ',') }}
 ```
 
 ### Filtering
-Inputs `allowed-languages` and `allowed-threshold` (language-lines/total lines) allow you to filter the resulting languages.
+
+Inputs `allowed-languages` and `allowed-threshold` (language-lines/total lines)
+allow you to filter the resulting languages.
+
 ```yml
-      - uses: IkerSaint/get_repo_languages@main
-        id: get-languages
-        with:
-          allowed-languages: "Javascript,Typescript,Java,C#,Golang,Rust,Python"
-          allowed-threshold: "10"
+- uses: IkerSaint/get_repo_languages@main
+  id: get-languages
+  with:
+    allowed-languages: 'Javascript,Typescript,Java,C#,Golang,Rust,Python'
+    allowed-threshold: '10'
 ```
 
 ## Outputs
--- src/main.js
+++ src/main.js
@@ -11,58 +11,58 @@ export async function run() {
     const token = core.getInput('gh-token')
     const owner = core.getInput('repo-owner')
     const repo = core.getInput('repo')
-    const allowed_languages_str = core.getInput('allowed-languages');
+    const allowed_languages_str = core.getInput('allowed-languages')
     const allowed_threshold = parseInt(core.getInput('allowed-threshold'))
 
-    core.info(`Initializing client`);
-    const gh_client = github.getOctokit(token);
+    core.info(`Initializing client`)
+    const gh_client = github.getOctokit(token)
 
-    core.info(`Pulling languages from ${owner}/${repo}`);
+    core.info(`Pulling languages from ${owner}/${repo}`)
 
-    const resp = await gh_client.request(`GET /repos/${owner}/${repo}/languages`);
+    const resp = await gh_client.request(
+      `GET /repos/${owner}/${repo}/languages`
+    )
 
-    let languages = resp.data;
+    let languages = resp.data
 
-    core.debug(`Got ${JSON.stringify(resp.data)}`);
+    core.debug(`Got ${JSON.stringify(resp.data)}`)
 
-    const total = Object.values(languages).reduce((accumulator, current) =>
-      accumulator + current,
+    const total = Object.values(languages).reduce(
+      (accumulator, current) => accumulator + current,
       0
-    );
+    )
 
-    core.debug(`Total lines ${total}`);
+    core.debug(`Total lines ${total}`)
 
     if (allowed_languages_str.trim().length > 0) {
-      let allowed = allowed_languages_str.split(',')
-        .map((x) =>
-          x.trim().toLowerCase()
-        );
+      let allowed = allowed_languages_str
+        .split(',')
+        .map((x) => x.trim().toLowerCase())
 
-      core.info(`Filtering for Languages in ${allowed}`);
+      core.info(`Filtering for Languages in ${allowed}`)
 
       Object.keys(languages)
-        .filter(key => !allowed.includes(key.toLowerCase()))
-        .forEach(key => delete languages[key]);
+        .filter((key) => !allowed.includes(key.toLowerCase()))
+        .forEach((key) => delete languages[key])
 
-      core.debug(`After language filter ${JSON.stringify(languages)}`);
+      core.debug(`After language filter ${JSON.stringify(languages)}`)
     }
 
     if (allowed_threshold > 0) {
-      core.info(`Filtering for threshold ${allowed_threshold}%`);
-      
-      Object.keys(languages)
-        .filter(key => (languages[key] / total) * 100 < allowed_threshold)
-        .forEach(key => delete languages[key]);
+      core.info(`Filtering for threshold ${allowed_threshold}%`)
 
-      core.debug(`After threshold filter ${JSON.stringify(languages)}`);
+      Object.keys(languages)
+        .filter((key) => (languages[key] / total) * 100 < allowed_threshold)
+        .forEach((key) => delete languages[key])
+
+      core.debug(`After threshold filter ${JSON.stringify(languages)}`)
     }
 
-    core.info(`Output ${JSON.stringify(languages)}`);
+    core.info(`Output ${JSON.stringify(languages)}`)
 
-    core.setOutput('languages', JSON.stringify(Object.keys(languages)));
+    core.setOutput('languages', JSON.stringify(Object.keys(languages)))
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error)
-      core.setFailed(error.message);
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
